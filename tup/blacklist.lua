39644aaecd0ab222d1e91bb3f38cce51a74b4ace
Gerd Zellweger <mail@gerdzellweger.com>
2018-05-03 12:56:40 -0700

    Get rid of old build system.
    Signed-off-by: Gerd Zellweger <mail@gerdzellweger.com>

--- tup/blacklist.lua
+++
@@ -1,8 +0,0 @@
--- Ban certain targets from being built
--- Note: This is copied into your source base dir by bootstrap.sh
--- and should be edited there (not the template in tools/tup/)
-blacklist = {}
-
-if tup.getconfig("BLACKLISTING_ENABLED") == "y" then
-
-end
--- usr/init/Tupfile.lua
+++
@@ -1,6 +0,0 @@
-buildRustApplication{
-    target = "init",
-    rsFile = "init.rs",
-    assemblyFiles = {"crt0.S"},
-    addLibraries = {"core", "x86"}
-}

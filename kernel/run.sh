324d2ee17db520838b41a29c283e828850f15600
Gerd Zellweger <mail@gerdzellweger.com>
2018-07-13 21:54:26 -0700

    Update scripts.
    Signed-off-by: Gerd Zellweger <mail@gerdzellweger.com>

--- kernel/run.sh
+++ kernel/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -ex
+
 ld -n --gc-sections -Tsrc/arch/x86_64/link.ld -o kernel ../target/bespin/debug/libbespin.a
 objcopy kernel -F elf32-i386 mbkernel
 qemu-system-x86_64 -m 1024 -d int -smp 1 -kernel ./mbkernel -nographic
--- setup.sh
+++ setup.sh
@@ -1,12 +1,28 @@
 #!/usr/bin/env bash
+set -ex
 
-# Install required packages
-sudo apt-get update
-sudo apt-get install qemu qemu-kvm
+# Install QEMU
+if [ ! -x "$(command -v qemu-system-x86_64)"]; then
+    if [ "$(uname)" == "Darwin" ]; then
+        brew install qemu
+    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+        sudo apt-get update
+        sudo apt-get install qemu qemu-kvm
+    fi
+fi
 
-curl https://sh.rustup.rs -sSf | sh -s -- -y
+
+# Make sure rust is up-to-date
+if [ ! -x "$(command -v rustup)" ] ; then
+    curl https://sh.rustup.rs -sSf | sh -s -- -y
+    source $HOME/.cargo/env
+fi
 source $HOME/.cargo/env
 rustup default nightly
 rustup component add rust-src
 rustup update
-cargo install xargo
+
+# Install xargo
+if [ ! -x "$(command -v xargo)" ]; then 
+    cargo install xargo
+fi

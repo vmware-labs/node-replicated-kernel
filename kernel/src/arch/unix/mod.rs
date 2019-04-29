0f3afd194b8b6ac646a9f48980f6d61620234f11
Gerd Zellweger <mail@gerdzellweger.com>
2019-04-29 14:04:37 -0700

    Slight reordering to allow more logging.

--- kernel/src/arch/unix/mod.rs
+++ kernel/src/arch/unix/mod.rs
@@ -25,15 +25,14 @@ pub mod debug {
 #[start]
 #[no_mangle]
 fn arch_init(_argc: isize, _argv: *const *const u8) -> isize {
-    // Note anything other than error currently broken
+    // Note anything lower than Info is currently broken
     // because macros in mem management will do a recursive
     // allocation and this stuff is not reentrant...
-    klogger::init(Level::Error).expect("Can't set-up logging");
-    info!(
-        "Started at {} with {:?} since CPU startup",
-        *rawtime::WALL_TIME_ANCHOR,
-        *rawtime::BOOT_TIME_ANCHOR
-    );
+    klogger::init(Level::Info).expect("Can't set-up logging");
+
+    lazy_static::initialize(&rawtime::WALL_TIME_ANCHOR);
+    lazy_static::initialize(&rawtime::BOOT_TIME_ANCHOR);
+
 
     // Allocate 32 MiB and add it to our heap
     let mut mb = BuddyFrameAllocator::new();
@@ -50,6 +49,12 @@ fn arch_init(_argc: isize, _argv: *const *const u8) -> isize {
 
     let mut kcb = kcb::Kcb::new(mb);
     kcb::init_kcb(kcb);
+
+    info!(
+        "Started at {} with {:?} since CPU startup",
+        *rawtime::WALL_TIME_ANCHOR,
+        *rawtime::BOOT_TIME_ANCHOR
+    );
 
     trace!("setting the kcb");
 

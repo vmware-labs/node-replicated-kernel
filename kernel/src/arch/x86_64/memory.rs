e99bd223211202c4bfa97a34e4c16c11e39e97f2
Gerd Zellweger <mail@gerdzellweger.com>
2019-05-11 14:40:52 -0700

    Make KERNEL_OFFSET public.

--- kernel/src/arch/x86_64/memory.rs
+++ kernel/src/arch/x86_64/memory.rs
@@ -1,7 +1,10 @@
+//! Function and definitions that are specific to how the
+//! x86-64 address space is laid out.
+
 pub use x86::bits64::paging::{PAddr, VAddr, BASE_PAGE_SIZE};
 
 /// Start of the kernel address space.
-const KERNEL_BASE: u64 = 0x400000000000;
+pub const KERNEL_BASE: u64 = 0x400000000000;
 
 /// Translate a kernel 'virtual' address to the physical address of the memory.
 pub fn kernel_vaddr_to_paddr(v: VAddr) -> PAddr {

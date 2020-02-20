79004c3cb4a8771cf6e34baf7e9a6edbd4d4fc76
Ankit Bhardwaj <bhrdwj.ankit@gmail.com>
2020-02-19 18:23:14 -0700

    Add UserPtr and UserValue to unix arch.

--- kernel/src/arch/unix/process.rs
+++ kernel/src/arch/unix/process.rs
@@ -1,5 +1,8 @@
 //! A dummy process implementation for the unix platform.
 use alloc::boxed::Box;
+use core::ops::{Deref, DerefMut};
+use x86::bits64::paging::*;
+use x86::bits64::rflags;
 
 use crate::arch::Module;
 use crate::fs::Fd;
@@ -7,6 +10,85 @@ use crate::memory::Frame;
 use crate::process::{Executor, Pid, Process, ProcessError, ResumeHandle};
 
 use super::vspace::VSpace;
+
+/// TODO: This code is same as x86_64 process. Can we remove it?
+pub struct UserPtr<T> {
+    value: *mut T,
+}
+
+impl<T> UserPtr<T> {
+    pub fn new(pointer: *mut T) -> UserPtr<T> {
+        UserPtr { value: pointer }
+    }
+
+    pub fn vaddr(&self) -> VAddr {
+        VAddr::from(self.value as u64)
+    }
+}
+
+impl<T> Deref for UserPtr<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        unsafe {
+            rflags::stac();
+            &*self.value
+        }
+    }
+}
+
+impl<T> DerefMut for UserPtr<T> {
+    fn deref_mut(&mut self) -> &mut T {
+        unsafe {
+            rflags::stac();
+            &mut *self.value
+        }
+    }
+}
+
+impl<T> Drop for UserPtr<T> {
+    fn drop(&mut self) {
+        unsafe { rflags::clac() };
+    }
+}
+
+pub struct UserValue<T> {
+    value: T,
+}
+
+impl<T> UserValue<T> {
+    pub fn new(pointer: T) -> UserValue<T> {
+        UserValue { value: pointer }
+    }
+
+    pub fn as_mut_ptr(&mut self) -> *mut T {
+        unsafe { core::mem::transmute(&self.value) }
+    }
+}
+
+impl<T> Deref for UserValue<T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        unsafe {
+            rflags::stac();
+            &self.value
+        }
+    }
+}
+
+impl<T> DerefMut for UserValue<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe {
+            rflags::stac();
+            &mut self.value
+        }
+    }
+}
+
+impl<T> Drop for UserValue<T> {
+    fn drop(&mut self) {
+        unsafe { rflags::clac() };
+    }
+}
 
 pub struct UnixProcess {
     vspace: VSpace,

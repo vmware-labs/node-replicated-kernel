aea74c02c40a71616ab518468efe961013767fbb
Gerd Zellweger <gzellweger@vmware.com>
2020-03-24 00:55:48 -0700

    Make mutex SMP aware.

--- lib/lineup/src/mutex.rs
+++ lib/lineup/src/mutex.rs
@@ -1,8 +1,10 @@
 use core::cell::UnsafeCell;
+use core::sync::atomic::{spin_loop_hint, AtomicUsize, Ordering};
 
 use crate::tls::Environment;
 use crate::{ds, Scheduler, ThreadId, ThreadState};
 
+use crossbeam_utils::CachePadded;
 use log::*;
 
 #[derive(Debug)]
@@ -21,6 +23,7 @@ impl Mutex {
                 is_spin: is_spin,
                 waitlist: ds::Vec::with_capacity(Scheduler::MAX_THREADS),
                 lwp_ptr: None,
+                counter: CachePadded::new(AtomicUsize::new(0)),
             }),
         }
     }
@@ -68,61 +71,100 @@ struct MutexInner {
     is_kmutex: bool,
     is_spin: bool,
     lwp_ptr: Option<*const u64>,
+    counter: CachePadded<AtomicUsize>,
 }
 
 impl MutexInner {
+    // SMP ready
     fn try_enter(&mut self) -> bool {
         let tid = Environment::tid();
         assert!(self.owner != Some(tid), "Locking mutex against itself.");
 
-        if self.owner.is_none() {
-            self.owner = Some(tid);
-            let thread_state = crate::tls::Environment::thread();
-            self.lwp_ptr = Some(thread_state.rump_lwp);
-            true
-        } else {
-            trace!(
-                "Mutex {:p} try_enter failed by {:?}, currently owned by {:?}",
-                self,
-                crate::tls::Environment::tid(),
-                self.owner
-            );
-            false
+        let counter = self.counter.load(Ordering::Relaxed);
+        loop {
+            if counter != 0 {
+                // Lock currently held by another thread
+                trace!(
+                    "Mutex {:p} try_enter failed by {:?}, currently owned by {:?}",
+                    self,
+                    crate::tls::Environment::tid(),
+                    self.owner
+                );
+                return false;
+            }
+
+            // Try to acquire it (set to 1):
+            if self.counter.compare_and_swap(0, 1, Ordering::Relaxed) == 0 {
+                // we hold the lock now
+                break;
+            }
+            // else: failed to acquire, retry
         }
+
+        let thread_state = crate::tls::Environment::thread();
+        self.owner = Some(tid);
+        self.lwp_ptr = Some(thread_state.rump_lwp);
+        true
     }
 
+    // SMP ready [1 TODO!]
     fn enter(&mut self) {
         let tid = Environment::tid();
         let yielder: &mut ThreadState = Environment::thread();
 
-        if !self.try_enter() {
+        if self.is_spin {
+            self.enter_nowrap();
+            return;
+        }
+
+        assert!(self.is_kmutex);
+        if self.counter.fetch_add(1, Ordering::SeqCst) != 0 {
+            // Didn't get the lock:
             let mut rid = 0;
             trace!("try_enter failed deschedule");
             (yielder.upcalls.deschedule)(&mut rid, None);
-            self.waitlist.push(tid);
+            self.waitlist.push(tid); // TODO: this needs to be atomic?
             yielder.make_unrunnable(tid);
-            assert!(self.try_enter());
             (yielder.upcalls.schedule)(&rid, None)
         }
+        // Acquired the lock
+        self.owner = Some(tid);
+        self.lwp_ptr = Some(yielder.rump_lwp);
     }
 
+    // SMP Ready
     fn enter_nowrap(&mut self) {
-        assert!(
-            self.try_enter(),
-            "one VCPU supported, no preemption so it must succeed"
-        );
+        loop {
+            // Wait till lock is free (counter is 0):
+            while self.counter.load(Ordering::Relaxed) != 0 {
+                spin_loop_hint();
+            }
+
+            // Try to acquire it (set to 1):
+            if self.counter.compare_and_swap(0, 1, Ordering::Relaxed) == 0 {
+                // continue, we hold the lock now
+                break;
+            }
+            // else: failed to acquire, retry
+        }
+
+        let tid = Environment::tid();
+        let thread_state = Environment::thread();
+        // Acquired the lock
+        self.owner = Some(tid);
+        self.lwp_ptr = Some(thread_state.rump_lwp);
     }
 
+    /// SMP ready [1 TODO!]
     fn exit(&mut self) {
         let tid = Environment::tid();
         let yielder: &mut ThreadState = Environment::thread();
 
-        //assert!(self.owner == Some(tid), "Only owner can exit mutex.");
         self.owner = None;
         self.lwp_ptr = None;
-
-        if !self.waitlist.is_empty() {
-            let next = self.waitlist.pop();
+        if self.counter.fetch_sub(1, Ordering::SeqCst) != 1 {
+            assert!(!self.waitlist.is_empty());
+            let next = self.waitlist.pop(); // TODO: this may have to be atomic...
             yielder.make_runnable(next.unwrap());
         }
     }
@@ -142,13 +184,13 @@ impl Drop for MutexInner {
 
 #[test]
 fn test_mutex() {
-    env_logger::init();
+    let _r = env_logger::try_init();
 
     use crate::DEFAULT_UPCALLS;
     use core::ptr;
 
     let mut s = Scheduler::new(DEFAULT_UPCALLS);
-    let mtx = ds::Arc::new(Mutex::new(false, false));
+    let mtx = ds::Arc::new(Mutex::new(false, true));
     let m1: ds::Arc<Mutex> = mtx.clone();
     let m2: ds::Arc<Mutex> = mtx.clone();
 

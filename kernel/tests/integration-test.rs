8676851136a439859fe020a97d4b108aa532d5be
Gerd Zellweger <mail@gerdzellweger.com>
2020-04-15 16:29:30 -0700

    Reduce amount of cores in vmops by default.

--- kernel/tests/integration-test.rs
+++ kernel/tests/integration-test.rs
@@ -1137,7 +1137,7 @@ fn s06_redis_benchmark_e1000() {
 
 #[test]
 fn s06_vmops_benchmark() {
-    for cores in 1..=12 {
+    for cores in 1..=4 {
         let cmdline = RunnerArgs::new("test-userspace-smp")
             .module("init")
             .user_feature("bench-vmops")

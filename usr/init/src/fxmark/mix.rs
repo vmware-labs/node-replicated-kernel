4822f18a903e59d5d4e67374ecbba7fc08f8f350
Ankit Bhardwaj <bhrdwj.ankit@gmail.com>
2020-05-20 17:22:55 -0400

    MIX: Read or write any random page instead of private area.

--- usr/init/src/fxmark/mix.rs
+++ usr/init/src/fxmark/mix.rs
@@ -10,9 +10,7 @@ use x86::random::rdrand16;
 #[derive(Clone)]
 pub struct MIX {
     page: Vec<u8>,
-    fd: RefCell<u64>,
     size: i64,
-    total_cores: RefCell<usize>,
 }
 
 impl Default for MIX {
@@ -28,16 +26,13 @@ impl Default for MIX {
 
         MIX {
             page,
-            fd: RefCell::new(u64::MAX),
-            size: 128 * 1024 * 1024,
-            total_cores: RefCell::new(0),
+            size: 256 * 1024 * 1024,
         }
     }
 }
 
 impl Bench for MIX {
-    fn init(&self, cores: Vec<usize>) {
-        *self.total_cores.borrow_mut() = cores.len();
+    fn init(&self, _cores: Vec<usize>) {
         unsafe {
             let fd = vibrio::syscalls::Fs::open(
                 "file.txt\0".as_ptr() as u64,
@@ -50,7 +45,9 @@ impl Bench for MIX {
                 vibrio::syscalls::Fs::write_at(fd, self.page.as_ptr() as u64, PAGE_SIZE, self.size)
                     .expect("FileWriteAt syscall failed");
             assert_eq!(ret, PAGE_SIZE as u64);
-            *self.fd.borrow_mut() = fd;
+            if vibrio::syscalls::Fs::close(fd).expect("FileClose syscall failed") != 0 {
+                panic!("FileClose syscall failed");
+            };
         }
     }
 
@@ -64,21 +61,13 @@ impl Bench for MIX {
         use vibrio::io::*;
         use vibrio::syscalls::*;
         let mut iops_per_second = Vec::with_capacity(duration as usize);
-        let fd = *self.fd.borrow();
-        if fd == u64::MAX {
-            panic!("Unable to open a file");
-        }
-        let total_cores = *self.total_cores.borrow();
+        let fd = vibrio::syscalls::Fs::open(
+            "file.txt\0".as_ptr() as u64,
+            u64::from(FileFlags::O_RDWR | FileFlags::O_CREAT),
+            u64::from(FileModes::S_IRWXU),
+        )
+        .expect("FileOpen syscall failed");
         let total_pages: usize = self.size as usize / 4096;
-        let pages_per_core = total_pages / total_cores;
-        let lower;
-        if core == 0 {
-            lower = 0;
-        } else {
-            lower = (core - 1) * pages_per_core;
-        }
-        let higher = core * pages_per_core;
-
         let page: &mut [i8; PAGE_SIZE as usize] = &mut [0; PAGE_SIZE as usize];
 
         // Synchronize with all cores
@@ -96,7 +85,7 @@ impl Bench for MIX {
             while start.elapsed().as_secs() < 1 {
                 for i in 0..64 {
                     unsafe { rdrand16(&mut random_num) };
-                    let rand = lower + (random_num as usize % pages_per_core);
+                    let rand = random_num as usize % total_pages;
                     let offset = rand * 4096;
 
                     if random_num as usize % 100 < write_ratio {

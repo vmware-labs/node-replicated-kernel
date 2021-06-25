e8a52ad51eef09696a31350aa07175c9722f847a
Gerd Zellweger <mail@gerdzellweger.com>
2021-06-25 12:31:03 -0700

    Fix kvm check for run.py.

--- kernel/run.py
+++ kernel/run.py
@@ -610,7 +610,7 @@ if __name__ == '__main__':
 
     user = whoami().strip()
     kvm_members = getent['group', 'kvm']().strip().split(":")[-1].split(',')
-    if not user in kvm_members:
+    if not user in kvm_members and not args.norun:
         print("Your user ({}) is not in the kvm group.".format(user))
         print("Add yourself to the group with `sudo adduser {} kvm`".format(user))
         print("You'll likely have to restart for changes to take effect,")

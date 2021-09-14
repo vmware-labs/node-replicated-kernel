b70fd501299207a74997a3b3c87a9c5c76273a3d
Erika Hunhoff <hunhoffe@vmware.com>
2021-09-14 07:46:53 -0700

    Continued to stub out MPSC test implementations

--- lib/rpc/tests/common/mpsc_client.rs
+++ lib/rpc/tests/common/mpsc_client.rs
@@ -1,14 +1,21 @@
 use std::fmt::Error;
+use std::sync::mpsc::{SyncSender, Receiver};
 
 use rpc::cluster_api::{ClusterClientAPI, ClusterError, NodeId};
 use rpc::rpc::*;
 use rpc::rpc_api::RPCClientAPI;
 
-pub struct MPSCClient {}
+pub struct MPSCClient {
+    rx : Receiver<Vec<u8>>,
+    tx : SyncSender<Vec<u8>>,
+}
 
 impl MPSCClient {
-    pub fn new() -> MPSCClient {
-        MPSCClient {}
+    pub fn new(rx : Receiver<Vec<u8>>, tx : SyncSender<Vec<u8>>) -> MPSCClient {
+        MPSCClient {
+            rx : rx,
+            tx : tx,
+        }
     }
 }
 
@@ -27,16 +34,20 @@ impl RPCClientAPI for MPSCClient {
         rpc_id: RPCType,
         data: Vec<u8>,
     ) -> Result<Vec<u8>, RPCError> {
-        Err(RPCError::NotSupported)
+        // TODO create rpc header
+        // TODO: concat header w/ data
+        // TODO: send data with header
+        self.msg_send(data)?;
+        self.msg_recv(0)
     }
 
     /// send data to a remote node
     fn msg_send(&mut self, data: Vec<u8>) -> Result<(), RPCError> {
-        Err(RPCError::NotSupported)
+        self.tx.send(data).map_err(|my_err| { RPCError::TransportError} )
     }
 
     /// receive data from a remote node
-    fn msg_recv(&mut self, expected_data: usize) -> Result<Vec<u8>, RPCError> {
-        Err(RPCError::NotSupported)
+    fn msg_recv(&mut self, _expected_data: usize) -> Result<Vec<u8>, RPCError> {
+        self.rx.recv().map_err(|my_err| { RPCError::TransportError} )
     }
 }

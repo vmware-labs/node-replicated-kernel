987e6f2694b40758c56f01d6df0c0fa64dacc1ef
Erika Hunhoff <hunhoffe@vmware.com>
2021-09-29 10:20:08 -0700

    Still fiddling with handler function signature

--- lib/rpc/src/cluster_api.rs
+++ lib/rpc/src/cluster_api.rs
@@ -1,7 +1,10 @@
 // Copyright © 2021 University of Colorado. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
+use alloc::vec::Vec;
 use core::result::Result;
+
+use crate::rpc::{RPCHeader, RPCType};
 
 #[derive(Debug, PartialEq, Eq, Copy, Clone)]
 pub enum ClusterError {
@@ -13,9 +16,15 @@ pub enum ClusterError {
 /// Node ID for servers/clients
 pub type NodeId = u64;
 
+pub const CLUSTER_OPERATION: RPCType = 0;
+
+pub fn is_reserved(rpc_id: RPCType) -> bool {
+    return rpc_id == CLUSTER_OPERATION;
+}
+
 pub trait ClusterControllerAPI {
     ///  Controller-side implementation for LITE join_cluster()
-    fn add_client(&mut self) -> Result<NodeId, ClusterError>;
+    fn add_client(&mut self, hdr: RPCHeader, payload: Vec<u8>) -> Result<NodeId, ClusterError>;
 }
 
 pub trait ClusterClientAPI {

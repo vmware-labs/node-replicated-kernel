3797565a52717816c1f73526bd7e43f862e2dcb2
Gerd Zellweger <gzellweger@vmware.com>
2021-12-10 14:03:12 -0800

    Fix missing import.

--- kernel/src/arch/x86_64/mod.rs
+++ kernel/src/arch/x86_64/mod.rs
@@ -181,6 +181,7 @@ unsafe fn find_current_ptables() -> PageTable {
 
 /// Construct the driver object to manipulate the interrupt controller (XAPIC)
 fn init_apic() -> x2apic::X2APICDriver {
+    use driverkit::DriverControl;
     let mut apic = x2apic::X2APICDriver::default();
     // Attach the driver to take control of the APIC:
     apic.attach();

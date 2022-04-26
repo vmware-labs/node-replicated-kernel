2ace9a3fd42bbba5d2bf3c15be6900b7eabbff4c
Ankit Bhardwaj <bhrdwj.ankit@gmail.com>
2022-04-26 13:07:17 -0400

    Create bidirectional transport using MPMC Queue.

--- lib/rpc/src/transport/shmem/mod.rs
+++ lib/rpc/src/transport/shmem/mod.rs
@@ -3,6 +3,7 @@
 
 use alloc::alloc::Allocator;
 
+use alloc::sync::Arc;
 mod allocator;
 pub mod transport;
 
@@ -10,17 +11,19 @@ mod queue_mpmc;
 use queue_mpmc::Queue;
 
 #[repr(transparent)]
-pub struct Sender<'a, T>(Queue<'a, T>);
+pub struct Sender<'a, T>(Arc<Queue<'a, T>>);
 
 unsafe impl<'a, T: Send> Send for Sender<'a, T> {}
 unsafe impl<'a, T: Sync> Sync for Sender<'a, T> {}
 
 impl<'a, T: Send + Clone> Sender<'a, T> {
     pub fn with_capacity_in<A: Allocator>(capacity: usize, alloc: A) -> Sender<'a, T> {
-        Sender(Queue::<T>::with_capacity_in(false, capacity, alloc).unwrap())
+        Sender(Arc::new(
+            Queue::<T>::with_capacity_in(false, capacity, alloc).unwrap(),
+        ))
     }
 
-    pub fn with_shared_queue(q: &'a Queue<T>) -> Sender<'a, T> {
+    pub fn with_shared_queue(q: Arc<Queue<'a, T>>) -> Sender<'a, T> {
         Sender(q.clone())
     }
 
@@ -35,17 +38,19 @@ impl<'a, T: Send + Clone> Sender<'a, T> {
 }
 
 #[repr(transparent)]
-pub struct Receiver<'a, T>(Queue<'a, T>);
+pub struct Receiver<'a, T>(Arc<Queue<'a, T>>);
 
 unsafe impl<'a, T: Send> Send for Receiver<'a, T> {}
 unsafe impl<'a, T: Sync> Sync for Receiver<'a, T> {}
 
 impl<'a, T: Send> Receiver<'a, T> {
     pub fn with_capacity_in<A: Allocator>(capacity: usize, alloc: A) -> Receiver<'a, T> {
-        Receiver(Queue::<T>::with_capacity_in(true, capacity, alloc).unwrap())
+        Receiver(Arc::new(
+            Queue::<T>::with_capacity_in(true, capacity, alloc).unwrap(),
+        ))
     }
 
-    pub fn with_shared_queue(q: &'a Queue<T>) -> Receiver<'a, T> {
+    pub fn with_shared_queue(q: Arc<Queue<'a, T>>) -> Receiver<'a, T> {
         Receiver(q.clone())
     }
 
@@ -68,9 +73,9 @@ mod tests {
     fn shared_queue_tests() {
         use super::*;
 
-        let queue = Queue::new().unwrap();
-        let sender = Sender::with_shared_queue(&queue);
-        let receiver = Receiver::with_shared_queue(&queue);
+        let queue = Arc::new(Queue::new().unwrap());
+        let sender = Sender::with_shared_queue(queue.clone());
+        let receiver = Receiver::with_shared_queue(queue.clone());
 
         let send_data: [u8; 10] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
         sender.send(send_data);

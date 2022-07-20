dc27f1fd0ff350b5831528eb571dcb8a0746bdfc
Erika Hunhoff <erikah@vmware.com>
2022-07-20 14:48:46 -0700

    Move duplicated code into one function

--- kernel/src/arch/x86_64/rackscale/processops/core.rs
+++ kernel/src/arch/x86_64/rackscale/processops/core.rs
@@ -8,12 +8,8 @@ use kpi::FileOperation;
 use log::{debug, warn};
 use rpc::rpc::*;
 use rpc::RPCClient;
-use smoltcp::socket::UdpSocket;
-use smoltcp::time::Instant;
 
-use crate::transport::ethernet::ETHERNET_IFACE;
-
-use super::super::dcm::dcm_msg::{AllocAssignment, AllocRequest, AllocResponse};
+use super::super::dcm::dcm_msg::make_dcm_request;
 use super::super::dcm::DCM_INTERFACE;
 
 use super::super::kernelrpc::*;
@@ -82,69 +78,11 @@ pub(crate) fn handle_request_core(hdr: &mut RPCHeader, payload: &mut [u8]) -> Re
         }
     };
 
-    let req = AllocRequest {
-        application: 1, // TODO: what is application, in this context?
-        cores: 0,
-        memslices: 1, // TODO: map page_size to number of memslices
-    };
-    let mut res = AllocResponse { alloc_id: 0 };
-    let mut assignment = AllocAssignment {
-        alloc_id: 0,
-        node: 0,
-    };
-
-    // Send call, get allocation response in return
-    {
-        DCM_INTERFACE
-            .lock()
-            .client
-            .call(local_pid, 1, unsafe { &[req.as_bytes()] }, unsafe {
-                &mut [res.as_mut_bytes()]
-            })
-            .unwrap();
-        debug!("Received allocation id in response: {:?}", res.alloc_id);
-    }
-
-    let mut received_allocation = false;
-    while !received_allocation {
-        {
-            let mut my_iface = ETHERNET_IFACE.lock();
-            let socket = my_iface.get_socket::<UdpSocket>(DCM_INTERFACE.lock().udp_handle);
-            if socket.can_recv() {
-                match socket.recv_slice(unsafe { assignment.as_mut_bytes() }) {
-                    Ok((_, endpoint)) => {
-                        debug!(
-                            "Received assignment: {:?} to node {:?}",
-                            assignment.alloc_id, assignment.node
-                        );
-                        if assignment.alloc_id != res.alloc_id {
-                            warn!("AllocIds do not match!");
-                        }
-                        socket.send_slice(&[1u8], endpoint).unwrap();
-                        received_allocation = true;
-                    }
-                    Err(e) => {
-                        debug!("Received nothing? {:?}", e);
-                    }
-                }
-            }
-        }
-
-        match ETHERNET_IFACE.lock().poll(Instant::from_millis(
-            rawtime::duration_since_boot().as_millis() as i64,
-        )) {
-            Ok(_) => {}
-            Err(e) => {
-                warn!("poll error: {}", e);
-            }
-        }
-    }
-
-    debug!("Ready to send response!");
+    let node = make_dcm_request(local_pid, false);
 
     // Construct and return result
     let res = KernelRpcRes {
-        ret: convert_return(Ok((assignment.node, 0))),
+        ret: convert_return(Ok((node, 0))),
     };
     construct_ret(hdr, payload, res)
 }

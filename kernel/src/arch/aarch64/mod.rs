889ca42892862f53ddf6a4df608be5c821bd047c
Reto Achermann <achreto@gmail.com>
2022-10-03 14:39:05 -0700

    armv8: adding dummy print on kernel entry
    Signed-off-by: Reto Achermann <achreto@gmail.com>

--- kernel/src/arch/aarch64/mod.rs
+++ kernel/src/arch/aarch64/mod.rs
@@ -35,6 +35,8 @@ pub(crate) fn advance_fs_replica() {
     panic!("not yet implemented");
 }
 
+use core::ptr::{read_volatile, write_volatile};
+
 /// Entry function that is called from UEFI At this point we are in x86-64
 /// (long) mode, We have a simple GDT, our address space, and stack set-up. The
 /// argc argument is abused as a pointer ot the KernelArgs struct passed by
@@ -43,11 +45,33 @@ pub(crate) fn advance_fs_replica() {
 #[start]
 #[no_mangle]
 fn _start(argc: isize, _argv: *const *const u8) -> isize {
+    unsafe {
+        asm!("hvc #0");
+    }
 
+    const A: *mut u32 = (0xffff_0000_0900_0000) as *mut u32;
+    const B: *mut u32 = (0x09000000) as *mut u32;
+
+    unsafe {
+        write_volatile(A, 'a' as u32);
+        write_volatile(A, 'b' as u32);
+        write_volatile(A, 'v' as u32);
+        write_volatile(A, 'd' as u32);
+        write_volatile(A, 'e' as u32);
+        write_volatile(A, 'f' as u32);
+        write_volatile(A, 'g' as u32);
+
+        write_volatile(B, 'a' as u32);
+        write_volatile(B, 'b' as u32);
+        write_volatile(B, 'v' as u32);
+        write_volatile(B, 'd' as u32);
+        write_volatile(B, 'e' as u32);
+        write_volatile(B, 'f' as u32);
+        write_volatile(B, 'g' as u32);
+    }
 
     sprint!("\r\n");
     sprint!("Hello from the kernel!\r");
     sprint!("\r\n");
-
-    0
+    halt();
 }

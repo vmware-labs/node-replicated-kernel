5c27a406ad7aaf13f5a6f78746d959a18856e317
Erika Hunhoff <hunhoff.erika@gmail.com>
2023-04-04 03:29:05 +0000

    Tidy controller code

--- kernel/src/arch/x86_64/rackscale/controller.rs
+++ kernel/src/arch/x86_64/rackscale/controller.rs
@@ -4,66 +4,38 @@
 use alloc::boxed::Box;
 use alloc::sync::Arc;
 use alloc::vec::Vec;
-use core::cell::Cell;
 use fallible_collections::FallibleVecGlobal;
-use hashbrown::HashMap;
-use lazy_static::lazy_static;
-use log::{debug, error, warn};
 use smoltcp::time::Instant;
-use spin::Mutex;
-use static_assertions as sa;
 
 use rpc::api::RPCServer;
 use rpc::rpc::RPCType;
 use rpc::server::Server;
+use rpc::transport::TCPTransport;
 
-use crate::arch::debug::shutdown;
-use crate::arch::rackscale::dcm::*;
-use crate::arch::rackscale::processops::request_core::RequestCoreReq;
 use crate::cmdline::Transport;
-use crate::error::KError;
-use crate::memory::backends::AllocatorStatistics;
-use crate::memory::mcache::MCache;
-use crate::memory::LARGE_PAGE_SIZE;
-use crate::nr;
-use crate::process::Pid;
 use crate::transport::ethernet::ETHERNET_IFACE;
-use crate::ExitReason;
+use crate::transport::shmem::create_shmem_transport;
 
 use super::*;
 
 const PORT: u16 = 6970;
 
-/// Test TCP RPC-based controller
+/// Controller main method
 pub(crate) fn run() {
-    // Create network interface and clock
-    #[derive(Debug)]
-    #[cfg_attr(feature = "defmt", derive(defmt::Format))]
-    pub(crate) struct Clock(Cell<Instant>);
-
-    impl Clock {
-        fn new() -> Clock {
-            let rt = rawtime::Instant::now().as_nanos();
-            let rt_millis = (rt / 1_000_000) as i64;
-            Clock(Cell::new(Instant::from_millis(rt_millis)))
-        }
-
-        fn elapsed(&self) -> Instant {
-            self.0.get()
-        }
-    }
-    let clock = Clock::new();
-
-    // Initialize the RPC server
+    // Initialize one server per client
     let num_clients = *crate::environment::NUM_MACHINES - 1;
     let mut servers: Vec<Box<dyn RPCServer<ControllerState>>> =
         Vec::try_with_capacity(num_clients as usize)
             .expect("Failed to allocate vector for RPC server");
+
     if crate::CMDLINE
         .get()
         .map_or(false, |c| c.transport == Transport::Ethernet)
     {
-        use rpc::{server::Server, transport::TCPTransport};
+        if num_clients > 1 {
+            panic!("Ethernet transport only supports on client, currently");
+        }
+
         let transport = Box::try_new(
             TCPTransport::new(None, PORT, Arc::clone(&ETHERNET_IFACE))
                 .expect("Failed to create TCP transport"),
@@ -77,13 +49,13 @@ pub(crate) fn run() {
         .get()
         .map_or(false, |c| c.transport == Transport::Shmem)
     {
-        use crate::transport::shmem::create_shmem_transport;
         for machine_id in 1..=num_clients {
             let transport = Box::try_new(
                 create_shmem_transport(machine_id.try_into().unwrap())
                     .expect("Failed to create shmem transport"),
             )
             .expect("Out of memory during init");
+
             let mut server: Box<dyn RPCServer<ControllerState>> =
                 Box::try_new(Server::new(transport)).expect("Out of memory during init");
             register_rpcs(&mut server);
@@ -104,12 +76,17 @@ pub(crate) fn run() {
     // Start running the RPC server
     log::info!("Starting RPC server!");
     loop {
-        match ETHERNET_IFACE.lock().poll(Instant::from_millis(
-            rawtime::duration_since_boot().as_millis() as i64,
-        )) {
-            Ok(_) => {}
-            Err(e) => {
-                log::warn!("poll error: {}", e);
+        if crate::CMDLINE
+            .get()
+            .map_or(false, |c| c.transport == Transport::Ethernet)
+        {
+            match ETHERNET_IFACE.lock().poll(Instant::from_millis(
+                rawtime::duration_since_boot().as_millis() as i64,
+            )) {
+                Ok(_) => {}
+                Err(e) => {
+                    log::warn!("poll error: {}", e);
+                }
             }
         }
 
@@ -121,9 +98,6 @@ pub(crate) fn run() {
             controller_state = new_state;
         }
     }
-
-    // Shutdown
-    shutdown(ExitReason::Ok);
 }
 
 fn register_rpcs(server: &mut Box<dyn RPCServer<ControllerState>>) {

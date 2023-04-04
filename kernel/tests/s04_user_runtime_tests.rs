fa63e701d7654441ccad3dc6395250232480786e
Erika Hunhoff <hunhoff.erika@gmail.com>
2023-04-04 01:10:42 +0000

    Extend timeout on s04 userspace multicore

--- kernel/tests/s04_user_runtime_tests.rs
+++ kernel/tests/s04_user_runtime_tests.rs
@@ -33,7 +33,7 @@ fn s04_userspace_multicore() {
     let cmdline = RunnerArgs::new_with_build("userspace-smp", &build)
         .cores(num_cores)
         .memory(4096)
-        .timeout(60_000);
+        .timeout(120_000);
 
     let mut output = String::new();
     let mut qemu_run = || -> Result<WaitStatus> {

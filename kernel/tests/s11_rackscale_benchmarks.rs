5df12dc7fbd8a101acf15a91d5ae3b709c8d3cee
Erika Hunhoff <hunhoff.erika@gmail.com>
2023-05-02 09:02:32 -0700

    Add nrk baseline to rackscale vmops

--- kernel/tests/s11_rackscale_benchmarks.rs
+++ kernel/tests/s11_rackscale_benchmarks.rs
@@ -26,7 +26,8 @@ use testutils::helpers::{
     DHCP_ACK_MATCH_NRK2, SHMEM_PATH, SHMEM_SIZE,
 };
 use testutils::runner_args::{
-    log_qemu_out_with_name, wait_for_sigterm_or_successful_exit_no_log, RunnerArgs,
+    check_for_successful_exit, log_qemu_out_with_name, wait_for_sigterm_or_successful_exit_no_log,
+    RunnerArgs,
 };
 
 #[test]
@@ -310,6 +311,7 @@ fn s11_rackscale_shmem_vmops_benchmark() {
 
 #[cfg(not(feature = "baremetal"))]
 fn rackscale_vmops_benchmark(is_shmem: bool) {
+    use std::collections::HashSet;
     use std::sync::Arc;
     use std::thread::sleep;
     use std::time::Duration;
@@ -335,26 +337,119 @@ fn rackscale_vmops_benchmark(is_shmem: bool) {
         build.build()
     });
 
+    let build_baseline = Arc::new({
+        let mut build = BuildArgs::default()
+            .module("init")
+            .user_feature("bench-vmops")
+            .kernel_feature("shmem")
+            .kernel_feature("ethernet")
+            .release();
+        if cfg!(feature = "smoke") {
+            build = build.user_feature("smoke");
+        }
+        build.build()
+    });
+    let mut baseline_set = HashSet::new();
+
     // TODO(rackscale): assert that there are enough threads/nodes on the machine for these settings?
     let _machine = Machine::determine();
     let threads = [1, 2, 4];
     let max_cores = *threads.iter().max().unwrap();
 
     let num_clients = if is_shmem { vec![1, 2, 4] } else { vec![1] };
+    let max_clients = *num_clients.iter().max().unwrap();
 
     for i in 0..num_clients.len() {
         let nclients = num_clients[i];
-        setup_network(nclients + 1);
-
-        // TODO(rackscale): probably scale with nclients?
-        let shmem_size = SHMEM_SIZE;
-
-        let all_outputs = Arc::new(Mutex::new(Vec::new()));
 
         for &cores in threads.iter() {
             // TODO(rackscale): this is probably too high, but oh well.
             let timeout = 120_000 + 20000 * (cores + nclients) as u64;
 
+            // TODO(rackscale): probably scale with nclients?
+            let shmem_size = SHMEM_SIZE;
+            let all_outputs = Arc::new(Mutex::new(Vec::new()));
+
+            // Run baseline test if needed
+            let baseline_cores = cores * nclients;
+            if !baseline_set.contains(&baseline_cores) {
+                setup_network(1);
+                let mut shmem_server = spawn_shmem_server(SHMEM_PATH, shmem_size)
+                    .expect("Failed to start shmem server");
+
+                let baseline_cmdline = format!("initargs={}", baseline_cores);
+
+                let mut cmdline_baseline =
+                    RunnerArgs::new_with_build("userspace-smp", &build_baseline)
+                        .timeout(timeout)
+                        .shmem_size(shmem_size as usize)
+                        .shmem_path(SHMEM_PATH)
+                        .tap("tap0")
+                        .no_network_setup()
+                        .workers(1)
+                        .cores(max_cores * max_clients)
+                        .use_vmxnet3()
+                        .cmd(baseline_cmdline.as_str());
+
+                if cfg!(feature = "smoke") {
+                    cmdline_baseline = cmdline_baseline.memory(10 * 1024);
+                } else {
+                    cmdline_baseline = cmdline_baseline.memory(48 * 1024);
+                }
+
+                let mut output = String::new();
+                let mut qemu_run = |cores| -> Result<WaitStatus> {
+                    let mut p = spawn_nrk(&cmdline_baseline)?;
+
+                    // Parse lines like
+                    // `init::vmops: 1,maponly,1,4096,10000,1000,634948`
+                    // write them to a CSV file
+                    let expected_lines = if cfg!(feature = "smoke") {
+                        1
+                    } else {
+                        cores * 11
+                    };
+
+                    for _i in 0..expected_lines {
+                        let (prev, matched) = p.exp_regex(
+                            r#"init::vmops: (\d+),(.*),(\d+),(\d+),(\d+),(\d+),(\d+)"#,
+                        )?;
+                        output += prev.as_str();
+                        output += matched.as_str();
+
+                        // Append parsed results to a CSV file
+                        let write_headers = !Path::new(file_name).exists();
+                        let mut csv_file = OpenOptions::new()
+                            .append(true)
+                            .create(true)
+                            .open(file_name)
+                            .expect("Can't open file");
+                        if write_headers {
+                            let row = "git_rev,nclients,thread_id,benchmark,ncores,memsize,duration_total,duration,operations\n";
+                            let r = csv_file.write(row.as_bytes());
+                            assert!(r.is_ok());
+                        }
+
+                        let parts: Vec<&str> = matched.split("init::vmops: ").collect();
+                        let r = csv_file.write(format!("{},", env!("GIT_HASH")).as_bytes());
+                        assert!(r.is_ok());
+                        let r = csv_file.write(format!("{},", 0).as_bytes());
+                        assert!(r.is_ok());
+                        let r = csv_file.write(parts[1].as_bytes());
+                        assert!(r.is_ok());
+                        let r = csv_file.write("\n".as_bytes());
+                        assert!(r.is_ok());
+                    }
+                    output += p.exp_eof()?.as_str();
+                    p.process.exit()
+                };
+                check_for_successful_exit(&cmdline_baseline, qemu_run(baseline_cores), output);
+                let _ignore = shmem_server.send_control('c');
+                baseline_set.insert(baseline_cores);
+            }
+
+            // Now run rackscale test
+            setup_network(nclients + 1);
             let (tx, rx) = channel();
             let rx_mut = Arc::new(Mutex::new(rx));
 

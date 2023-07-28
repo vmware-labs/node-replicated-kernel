0e42045fc25f106b49b7e7ad6f07820709a8b451
Erika Hunhoff <hunhoff.erika@gmail.com>
2023-07-28 09:45:41 -0700

    increase client build delay

--- kernel/tests/s11_rackscale_benchmarks.rs
+++ kernel/tests/s11_rackscale_benchmarks.rs
@@ -608,7 +608,7 @@ fn rackscale_memcached_benchmark(is_shmem: bool) {
     test.use_affinity_shmem = cfg!(feature = "affinity-shmem");
     test.file_name = file_name.to_string();
     test.arg = Some(config);
-    test.client_build_delay *= 2;
+    test.client_build_delay *= 3;
     test.run_dhcpd_for_baseline = true;
 
     fn cmd_fn(num_cores: usize, arg: Option<MemcachedInternalConfig>) -> String {
@@ -699,7 +699,7 @@ fn rackscale_monetdb_benchmark(is_shmem: bool) {
     test.use_affinity_shmem = cfg!(feature = "affinity-shmem");
     test.file_name = file_name.to_string();
     test.arg = None;
-    test.client_build_delay *= 2;
+    test.client_build_delay *= 3;
     test.run_dhcpd_for_baseline = true;
 
     fn cmd_fn(num_cores: usize, arg: Option<MemcachedInternalConfig>) -> String {

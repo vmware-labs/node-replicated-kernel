dd84480be46ba593c68d372ac38c0d7583581ee8
Erika Hunhoff <54562339+hunhoffe@users.noreply.github.com>
2023-08-11 17:29:43 -0600

    Increase timeout for rackscale memcached internal

--- kernel/tests/s11_rackscale_benchmarks.rs
+++ kernel/tests/s11_rackscale_benchmarks.rs
@@ -627,7 +627,7 @@ fn rackscale_memcached_benchmark(transport: RackscaleTransport) {
     }
 
     fn rackscale_timeout_fn(num_cores: usize) -> u64 {
-        180_000 + 2_000 * num_cores as u64
+        240_000 + 5_000 * num_cores as u64
     }
 
     fn mem_fn(num_cores: usize, is_smoke: bool) -> usize {

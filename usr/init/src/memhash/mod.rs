182e4b9b60adb2660f77f839c1c5e525a1382fc5
zmckevitt <zack.mckevitt@gmail.com>
2023-09-29 17:43:30 -0500

    Added benchmark logging to csv

--- usr/init/src/memhash/mod.rs
+++ usr/init/src/memhash/mod.rs
@@ -1,3 +1,4 @@
+use alloc::sync::Arc;
 use alloc::vec::Vec;
 use core::ptr;
 
@@ -16,7 +17,7 @@ fn hashmem(core_id: usize) -> u64 {
     0
 }
 
-fn thread_routine(core_id: usize) {
+fn thread_routine(core_id: usize, cur_cores: usize, tot_cores: usize) {
     // Synchronize all cores
     POOR_MANS_BARRIER.fetch_sub(1, Ordering::Relaxed);
     while POOR_MANS_BARRIER.load(Ordering::Relaxed) != 0 {
@@ -30,16 +31,23 @@ fn thread_routine(core_id: usize) {
         let _ = hashmem(core_id);
         ops += 1
     }
-    info!(
-        "Core {:?} achieved {:?} operations per second",
-        core_id, ops
-    );
+    info!("{},memhash,{},{},{}", core_id, ops, cur_cores, tot_cores);
 }
 
-unsafe extern "C" fn thread_routine_trampoline(arg1: *mut u8) -> *mut u8 {
-    let core_id = arg1 as usize;
-    thread_routine(core_id);
+unsafe extern "C" fn thread_routine_trampoline(thread_params: *mut u8) -> *mut u8 {
+    let params = Arc::from_raw(thread_params as *const ThreadParams);
+
+    let core_id = params.core_id;
+    let cur_cores = params.cur_cores;
+    let tot_cores = params.tot_cores;
+    thread_routine(core_id, cur_cores, tot_cores);
     ptr::null_mut()
+}
+
+struct ThreadParams {
+    core_id: usize,
+    cur_cores: usize,
+    tot_cores: usize,
 }
 
 pub fn bench(ncores: Option<usize>) {
@@ -58,7 +66,6 @@ pub fn bench(ncores: Option<usize>) {
             ) {
                 Ok(core_token) => {
                     core_ids.push(core_token.gtid());
-                    // continue;
                 }
                 Err(e) => {
                     error!("Can't spawn on {:?}: {:?}", hwthread.id, e);
@@ -69,24 +76,30 @@ pub fn bench(ncores: Option<usize>) {
             core_ids.push(hwthread.id);
         }
 
-        info!("Running memhash benchmark with cores: {:?}", core_ids);
+        // info!("Running memhash benchmark with cores: {:?}", core_ids);
 
         let cores_in_use = core_ids.len();
         let core_ids_copy = core_ids.clone();
 
         // Spawn threads
         s.spawn(
-            32 * 4096, // stack size, idk how much to allocate here
+            32 * 4096, // stack size, not sure how much to allocate here
             move |_| {
                 let mut thandles = Vec::with_capacity(cores_in_use.clone());
                 POOR_MANS_BARRIER.store(cores_in_use.clone(), Ordering::SeqCst);
 
                 for core_id in core_ids_copy {
+                    let params = ThreadParams {
+                        core_id: core_id,
+                        cur_cores: cores_in_use.clone(),
+                        tot_cores: ncores.unwrap().clone(),
+                    };
+
                     thandles.push(
                         Environment::thread()
                             .spawn_on_core(
                                 Some(thread_routine_trampoline),
-                                core_id as *mut u8,
+                                Arc::into_raw(params.into()) as *const _ as *mut u8,
                                 core_id,
                             )
                             .expect("Can't spawn bench thread"),

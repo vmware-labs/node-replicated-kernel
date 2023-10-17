04620acd4cfb5a9fe439ac2c2424f7805e4686ea
Reto Achermann <achreto@gmail.com>
2023-10-17 14:08:18 -0700

    tests: adding sharded memcached to the CI pipeline
    Signed-off-by: Reto Achermann <achreto@gmail.com>

--- scripts/ci.bash
+++ scripts/ci.bash
@@ -17,6 +17,7 @@ rm -f rackscale_shmem_vmops_benchmark.csv
 rm -f rackscale_shmem_vmops_latency_benchmark.csv
 rm -f rackscale_shmem_fxmark_benchmark.csv
 rm -f rackscale_shmem_memcached_benchmark.csv
+rm -f rackscale_sharded_memcached_benchmark_*.csv
 
 # For vmops: --features prealloc can improve performance further (at the expense of test duration)
 RUST_TEST_THREADS=1 cargo test --test s10* -- s10_vmops_benchmark --nocapture
@@ -30,6 +31,8 @@ RUST_TEST_THREADS=1 cargo test --test s11* -- s11_rackscale_shmem_vmops_maptput_
 RUST_TEST_THREADS=1 cargo test --test s11* -- s11_rackscale_shmem_vmops_maplat_benchmark --nocapture
 RUST_TEST_THREADS=1 cargo test --test s11* -- s11_rackscale_shmem_fxmark_bench --nocapture
 RUST_TEST_THREADS=1 cargo test --test s11* -- s11_rackscale_memcached_benchmark_internal --nocapture
+RUST_TEST_THREADS=1 cargo test --test s11* -- s11_rackscale_memcached_benchmark_sharded_linux --nocapture
+RUST_TEST_THREADS=1 cargo test --test s11* -- s11_rackscale_memcached_benchmark_sharded_nros --nocapture
 
 # Clone repo
 rm -rf gh-pages
@@ -61,7 +64,10 @@ if [ -d "${DEPLOY_DIR}" ]; then
 fi
 mkdir -p ${DEPLOY_DIR}
 mv memcached_benchmark_internal.csv ${DEPLOY_DIR}
+mv memcached_benchmark_sharded_*.csv ${DEPLOY_DIR}
 gzip ${DEPLOY_DIR}/memcached_benchmark_internal.csv
+gzip ${DEPLOY_DIR}/memcached_benchmark_sharded_nros.csv
+gzip ${DEPLOY_DIR}/memcached_benchmark_sharded_linux.csv
 
 # Copy vmops results
 DEPLOY_DIR="gh-pages/vmops/${CI_MACHINE_TYPE}/${GIT_REV_CURRENT}/"

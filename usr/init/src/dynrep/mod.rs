dbb54a870581f75514038b6532f34db6c1a4deba
Erika Hunhoff <hunhoff.erika@gmail.com>
2023-12-04 20:46:17 -0700

    Create hashmap

--- usr/init/src/dynrep/mod.rs
+++ usr/init/src/dynrep/mod.rs
@@ -1,7 +1,14 @@
 // Copyright © 2021 VMware, Inc. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR MIT
-use log::{error, info};
+use log::info;
+
+use hashbrown::HashMap;
 
 pub fn userspace_dynrep_test() {
+
+    // TODO: types are a placeholder
+    // TODO: will want to create with HashMap::with_capacity_in() method
+    let mut map: HashMap<usize, usize> = HashMap::new();
+
     info!("dynrep_test OK");
 }
